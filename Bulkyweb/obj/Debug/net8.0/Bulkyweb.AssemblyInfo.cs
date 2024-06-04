370aedcbd19e7d376599e7040f658e2b622c6fcf
divyanshraj467 <divyanshraj1@gmail.com>
2024-06-05 00:21:41 +0530

    commit

--- Bulkyweb/obj/Debug/net8.0/Bulkyweb.AssemblyInfo.cs
+++ Bulkyweb/obj/Debug/net8.0/Bulkyweb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulkyweb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41ca6e572ae11f8ddbe9ff612c379463182ae953")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulkyweb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulkyweb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
